4423677eb983ac6399a68a885b2ea620233236b3
natyosu3 <nachi33482@gmail.com>
2024-07-16 23:30:07 +0900

    単語テーブルのデフォルトを作成する機能の実装

---
+++ pkg/db/create/default.go
@@ -0,0 +1,26 @@
+package create
+
+import (
+	"FlickGameBack/pkg/db"
+	"log/slog"
+
+	_ "github.com/lib/pq"
+)
+
+// デフォルトのテーブルを作成する
+func CreateDefaultTable() {
+	db := db.Connect()
+	defer db.Close()
+
+	var sql_stm []string = []string{
+		`create table IF NOT EXISTS "word" (word_id text PRIMARY KEY, word_text text NOT NULL, word_level text, point_allocation int)`,
+	}
+
+	for _, stm := range sql_stm {
+		_, err := db.Exec(stm)
+		if err != nil {
+			slog.Error("failed to create table", "err=", err)
+		}
+	}
+	slog.Info("create default table")
+}
