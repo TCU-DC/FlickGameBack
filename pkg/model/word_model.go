14009a083fd08e8713cf3abaa42cf907b5b28c0a
natyosu3 <nachi334825@gmail.com>
2024-07-17 14:16:13 +0900

    feat: Add WordFurigana field to Word model

--- pkg/model/word_model.go
+++ pkg/model/word_model.go
@@ -3,6 +3,7 @@ package model
 type Word struct {
 	WordId          string `json:"word_id"`
 	WordText        string `json:"word_text"`
+	WordFurigana    string `json:"word_furigana"`
 	WordLevel       string `json:"word_level"`
 	PointAllocation int    `json:"point_allocation"`
 }
