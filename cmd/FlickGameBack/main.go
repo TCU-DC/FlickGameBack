0a5e6c62840e5ca93778a9179eb1d2bc84e0c326
natyosu3 <nachi334825@gmail.com>
2024-07-18 11:11:23 +0900

    feat: Add default word_data creation functionality (main)

--- cmd/FlickGameBack/main.go
+++ cmd/FlickGameBack/main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"FlickGameBack/pkg/db/create"
 	"FlickGameBack/pkg/engine"
+	"FlickGameBack/pkg/util"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func init() {
 	// デフォルトのテーブルを作成
 	create.CreateDefaultTable()
+
+	// デフォルトの単語を追加
+	util.AddWord()
+}
+
+func main() {
 	r := gin.Default()
 	r = engine.Engine(r)
 	r.Run(":8080")
