6a4c12d6fe5d6b30827b8b4aa561ac3d352dff66
natyosu3 <nachi334825@gmail.com>
2024-07-18 11:10:44 +0900

    feat: Refactor ReadWords function to return words with furigana

--- pkg/db/read/read.go
+++ pkg/db/read/read.go
@@ -7,24 +7,26 @@ import (
 )
 
 // 単語を取得する
-func ReadWords(level string, count int) []model.Word {
+func ReadWords(level string, count int) ([]model.Word, error) {
 	db := db.Connect()
 	defer db.Close()
 
 	var words []model.Word
-	rows, err := db.Query("SELECT * FROM word WHERE word_level = $1 ORDER BY RANDOM() LIMIT $2", level, count)
+	rows, err := db.Query("SELECT word_id, word_text, word_furigana, word_level, point_allocation FROM word WHERE word_level = $1 ORDER BY RANDOM() LIMIT $2", level, count)
 	if err != nil {
 		slog.Error("failed to select words", "err=", err)
+		return nil, err
 	}
 
 	for rows.Next() {
 		var word model.Word
-		err := rows.Scan(&word.WordId, &word.WordText, &word.WordLevel, &word.PointAllocation)
+		err := rows.Scan(&word.WordId, &word.WordText, &word.WordFurigana, &word.WordLevel, &word.PointAllocation)
 		if err != nil {
 			slog.Error("failed to scan word", "err=", err)
+			return nil, err
 		}
 		words = append(words, word)
 	}
 
-	return words
+	return words, nil
 }
