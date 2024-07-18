c4d73e5a2f3411c633580623ad56f0d362a9c26e
natyosu3 <nachi33482@gmail.com>
2024-07-19 02:54:17 +0900

    feat: Update InsertWord function to handle existing words

--- pkg/db/create/insert_word.go
+++ pkg/db/create/insert_word.go
@@ -2,6 +2,8 @@ package create
 
 import (
 	"FlickGameBack/pkg/db"
+	"FlickGameBack/pkg/db/read"
+	"FlickGameBack/pkg/db/update"
 	"FlickGameBack/pkg/model"
 	"log/slog"
 )
@@ -10,6 +12,14 @@ import (
 func InsertWord(word model.Word) error {
 	db := db.Connect()
 	defer db.Close()
+
+	// 既に存在する単語であれば更新する
+	if word_id, err := read.CheckExistWord(word.WordText); err == nil && word_id == word.WordId {
+		if err := update.UpdateWord(word); err != nil {
+			return err
+		}
+		return nil
+	}
 
 	_, err := db.Exec("INSERT INTO word (word_id, word_text, word_furigana, word_level, point_allocation) VALUES ($1, $2, $3, $4, $5)", word.WordId, word.WordText, word.WordFurigana, word.WordLevel, word.PointAllocation)
 	if err != nil {
