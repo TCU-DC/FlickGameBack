d954dd5e028ea89c22021850e071edcf6a0ca52b
natyosu3 <nachi33482@gmail.com>
2024-07-20 00:48:49 +0900

    feat: Add API endpoint to register user scores

--- pkg/engine/score/register_score.go
+++ pkg/engine/score/register_score.go
@@ -10,19 +10,36 @@ import (
 
 func RegisterScore() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var score model.Score
+		var req_score model.RequestScore
 		// リクエストボディをパース
-		c.BindJSON(&score)
+		c.BindJSON(&req_score)
 
 		// もしユーザIDが空文字ならゲストとして登録(nullも判定可能)
-		if score.UserID == "" {
-			score.UserID = "guest_" + util.RandomString(10)
+		if req_score.UserID == "" {
+			req_score.UserID = "guest_" + util.RandomString(10)
+
+			// ゲスト名が空文字ならゲスト名を設定
+			if req_score.GuestName == "" {
+				req_score.GuestName = "名無しさん"
+			}
+
+			// ゲストの場合はゲストユーザを登録
+			err := create.InsertUser(model.User{
+				UserID:        req_score.UserID,
+				UserName:      req_score.UserID,
+				NickName:      req_score.GuestName,
+				Password_hash: "",
+			})
+			if err != nil {
+				c.JSON(500, gin.H{"message": "failed to insert user"})
+				return
+			}
 		}
 
 		// 得点情報を登録
-		err := create.InsertScore(score.UserID, score.Point, score.AverageSpeed, score.Level)
+		err := create.InsertScore(req_score.UserID, req_score.Point, req_score.AverageSpeed, req_score.Level)
 		if err != nil {
-			c.JSON(500, gin.H{"message": "failed to insert score"})
+			c.JSON(500, gin.H{"message": "failed to insert req_score"})
 			return
 		}
 
