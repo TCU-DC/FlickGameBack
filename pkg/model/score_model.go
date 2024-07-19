0c671721d77a6f0ce5808a74a33951b4b9138913
natyosu3 <nachi33482@gmail.com>
2024-07-19 23:53:51 +0900

    feat: Add Score model to handle user scores

---
+++ pkg/model/score_model.go
@@ -0,0 +1,9 @@
+package model
+
+type Score struct {
+	ScoreID      int     `json:"score_id"`
+	UserID       string  `json:"user_id"`
+	Point        int     `json:"point"`
+	AverageSpeed float64 `json:"average_speed"`
+	Level        string  `json:"level"`
+}
