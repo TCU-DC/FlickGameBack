a39839e76bec5131345cdb50af4466f4b0a5feb5
natyosu3 <nachi33482@gmail.com>
2024-07-19 23:54:14 +0900

    Update word table schema

--- pkg/db/create/default.go
+++ pkg/db/create/default.go
@@ -13,7 +13,15 @@ func CreateDefaultTable() {
 	defer db.Close()
 
 	var sql_stm []string = []string{
-		`create table IF NOT EXISTS "word" (word_id text PRIMARY KEY, word_text text UNIQUE NOT NULL, word_furigana text NOT NULL, word_level text NOT NULL, point_allocation int NOT NULL)`,
+		`create table IF NOT EXISTS "level" (level_name text PRIMARY KEY)`,
+		`create table IF NOT EXISTS "score" (score_id serial PRIMARY KEY, user_id text, point int, average_speed float, level text, FOREIGN KEY (level) REFERENCES level(level_name))`,
+		`create table IF NOT EXISTS "word" (word_id text PRIMARY KEY, word_text text UNIQUE NOT NULL, word_furigana text NOT NULL, word_level text, point_allocation int NOT NULL, FOREIGN KEY (word_level) REFERENCES level(level_name))`,
+	}
+
+	// レベルのデフォルト値を挿入easy, normal, hard
+	_, err := db.Exec(`insert into level (level_name) values ('easy'), ('normal'), ('hard')`)
+	if err != nil {
+		slog.Error("failed to insert default level", "err=", err)
 	}
 
 	for _, stm := range sql_stm {
