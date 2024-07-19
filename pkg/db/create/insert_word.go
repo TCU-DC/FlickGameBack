0ac9dadd5aa0b7cec305f91adb59b314f290e51d
natyosu3 <nachi334825@gmail.com>
2024-07-19 09:28:50 +0900

    feat: Generate random ID for words in InsertWord function

--- pkg/db/create/insert_word.go
+++ pkg/db/create/insert_word.go
@@ -5,6 +5,7 @@ import (
 	"FlickGameBack/pkg/db/read"
 	"FlickGameBack/pkg/db/update"
 	"FlickGameBack/pkg/model"
+	"FlickGameBack/pkg/util"
 	"log/slog"
 )
 
@@ -14,14 +15,21 @@ func InsertWord(word model.Word) error {
 	defer db.Close()
 
 	// 既に存在する単語であれば更新する
-	if word_id, err := read.CheckExistWord(word.WordText); err == nil && word_id == word.WordId {
+	if word_id, err := read.CheckExistWord(word.WordText); err == nil {
+		// 単語のIDを設定
+		word.WordId = word_id
 		if err := update.UpdateWord(word); err != nil {
+			slog.Error("failed to update word", "err=", err)
 			return err
 		}
+		slog.Info("word already exists, updated", "word_id=", word.WordId)
 		return nil
 	}
 
-	_, err := db.Exec("INSERT INTO word (word_id, word_text, word_furigana, word_level, point_allocation) VALUES ($1, $2, $3, $4, $5)", word.WordId, word.WordText, word.WordFurigana, word.WordLevel, word.PointAllocation)
+	// 単語のIDを生成
+	word_id := util.RandomString(12)
+
+	_, err := db.Exec("INSERT INTO word (word_id, word_text, word_furigana, word_level, point_allocation) VALUES ($1, $2, $3, $4, $5)", word_id, word.WordText, word.WordFurigana, word.WordLevel, word.PointAllocation)
 	if err != nil {
 		slog.Error("failed to insert word", "err=", err)
 		return err
