a8c49a8cd71f7ec07f54be4ac52b9b14b231d38e
natyosu3 <nachi334825@gmail.com>
2024-07-20 12:12:07 +0900

    feat: Add API endpoint to retrieve ranking data

---
+++ pkg/engine/ranking/get_ranking.go
@@ -0,0 +1,40 @@
+package ranking
+
+import (
+	"FlickGameBack/pkg/db/read"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+// ランキングを取得するエンドポイントハンドラ
+func GetRanking() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// クエリパラメータを取得
+		level := c.Query("level")
+		count_str := c.Query("high_order")
+
+		// クエリパラメータがない場合はエラー
+		if level == "" || count_str == "" {
+			c.JSON(400, gin.H{"error": "level and count are required"})
+			return
+		}
+
+		// countパラメータをintに変換
+		count, err := strconv.Atoi(count_str)
+		if err != nil {
+			c.JSON(400, gin.H{"error": "count must be an integer"})
+			return
+		}
+
+		// DBからランキングを取得
+		ranking, err := read.ReadRanking(level, count)
+		if err != nil {
+			c.JSON(500, gin.H{"error": "failed to read ranking"})
+			return
+		}
+
+		// レスポンス
+		c.JSON(200, gin.H{"data": ranking})
+	}
+}
